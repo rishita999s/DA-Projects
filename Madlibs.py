1b0603b6acb1fdeb92a7b9b321697c7a16686565
Rishita_Sanghrajka <124574246+rishita999s@users.noreply.github.com>
2023-07-07 19:46:29 +0530

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,6 @@
+Noun=input("Noun: ")
+adjective=input("Adjective: ")
+Noun2=input("Noun2: ")
+madlib="It was pizza day at school, and Jamie was super " +adjective+ " for lunch. But when she went outside to eat, a " +Noun+ " stole her pizza! Jamie chased the " +Noun+ " all over school. She climbed, jumped, and ran through the playground."
+
+print(madlib)
